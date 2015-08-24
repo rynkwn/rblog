bd49e24ab04bdaf92c3ef5ba9e1b492c74716a1b
rynkwn <rynkwn@gmail.com>
2015-08-24 07:07:08 +0000

    Set up bcrypt and modified User model.

--- db/migrate/20150824070413_create_users.rb
+++ db/migrate/20150824070413_create_users.rb
@@ -1,6 +1,8 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
+      t.text :email
+      t.text :password_digest
 
       t.timestamps null: false
     end
