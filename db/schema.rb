5eb6d102b1fe7395e8527435b3fb9e386ccb5e7a
rynkwn <rynkwn@gmail.com>
2015-09-13 17:54:34 +0000

    Added ryan attribute to Users model. Continued work on Settings.

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,7 @@ ActiveRecord::Schema.define(version: 20150824070413) do
   create_table "users", force: :cascade do |t|
     t.text     "email"
     t.text     "password_digest"
+    t.integer  "ryan"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
   end
