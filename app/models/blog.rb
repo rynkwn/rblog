a190dcaac8cb36a48653a097d64ae786c77f7c50
rynkwn <rynkwn@gmail.com>
2015-09-18 18:49:32 +0000

    Set up validations for my three models.

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,2 +1,6 @@
 class Blog < ActiveRecord::Base
+  validates :name, presence: true
+  validates :subject_id, presence: true
+  
+  serialize :tags, Array  # Sets up Tags to be an array.
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,19 @@
 class User < ActiveRecord::Base
-    has_secure_password
+  before_save {self.email = email.downcase }
+  
+  validates :email, presence: true, uniqueness: {case_sensitive: false}
+  validates :password, length: {minimum: 6}
+  
+  has_secure_password
+  
+  after_initialize :init
     
-    after_initialize :init
-      
-    def init
-      self.ryan = self.email == 'rynkwn@gmail.com' ? 1 : 0
-      self.save!
-    end
-    
-    def ryan?
-      self.ryan == 1
-    end
+  def init
+    self.ryan = self.email == 'rynkwn@gmail.com' ? 1 : 0
+    self.save!
+  end
+  
+  def ryan?
+    self.ryan == 1
+  end
 end
