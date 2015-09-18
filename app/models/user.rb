57833c622f6a94bf7da478f85fbba1f2a983c2b7
rynkwn <rynkwn@gmail.com>
2015-09-18 18:23:47 +0000

    Updated User model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
 class User < ActiveRecord::Base
     has_secure_password
     
+    after_initialize :init
+      
+    def init
+      self.ryan = self.email == 'rynkwn@gmail.com' ? 1 : 0
+      self.save!
+    end
+    
     def ryan?
       self.ryan == 1
     end
