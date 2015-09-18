61f0fdc3dfa67494532a2374ede993c4cfd9500f
rynkwn <rynkwn@gmail.com>
2015-09-18 00:36:59 +0000

    Set up Titles, and basically fleshed out my About page.

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,2 +1,6 @@
-POSTMARK_API_KEY = "94252afc-de91-441e-9d0b-495a6ccadd42" # (Manually inserted into application.rb)
+# Application Constants
+NAME_OF_SITE = "aRg"
+
+# Postmark Constants
+POSTMARK_API_KEY = "94252afc-de91-441e-9d0b-495a6ccadd42"  # (Manually inserted into application.rb)
 SENDER_SIGNATURE = 'rhk1@williams.edu'
