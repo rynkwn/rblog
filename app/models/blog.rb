75d4c9613c3f67079263fec1a6c2201b2a0c04c0
rynkwn <rynkwn@gmail.com>
2015-09-21 03:20:04 +0000

    Refined Blog summarization. Working on the aesthetics of Subject Descriptions.

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -3,4 +3,12 @@ class Blog < ActiveRecord::Base
   validates :subject_id, presence: true
   
   serialize :tags, Array  # Sets up Tags to be an array.
+  
+  def summarize
+    if self.content.length >= 100
+      self.content[0..100] + "..."
+    else
+      self.content
+    end
+  end
 end
