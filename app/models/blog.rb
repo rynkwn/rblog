3b5d9bc0f2e1c3379c048401d018327b92e44231
rynkwn <rynkwn@gmail.com>
2015-09-21 23:51:46 +0000

    Wrote to_data functions.

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -4,6 +4,7 @@ class Blog < ActiveRecord::Base
   
   serialize :tags, Array  # Sets up Tags to be an array.
   
+  # Summarizes the blog's content in 100 characters or less.
   def summarize
     if self.content.length >= 100
       self.content[0..100] + "..."
@@ -11,4 +12,15 @@ class Blog < ActiveRecord::Base
       self.content
     end
   end
+  
+  # to_data converts the meaningful data of this object into a string, to be used
+  # for data_nukes.
+  def to_data
+    "Name: " + self.name + "\n" +
+    "Date Created: " + self.date_created.to_s + "\n" +
+    "Subject: " + Subject.find_by(id: self.subject_id).name + "\n" +
+    "Content: " + self.content + "\n" +
+    "Tags: " + self.tags.to_s
+  end
+  
 end
--- app/models/subject.rb
+++ app/models/subject.rb
@@ -2,4 +2,10 @@ class Subject < ActiveRecord::Base
   validates :name, presence: true
   
   has_many :blogs
+  
+  # to_data converts the meaningful data of this object into a string, to be used
+  # for data_nukes.
+  def to_data
+    "Name: " + self.name
+  end
 end
