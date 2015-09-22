e622d64a303997719fc4293da8621bb18617d476
rynkwn <rynkwn@gmail.com>
2015-09-22 00:12:57 +0000

    Should have written the correct data_nuke function now. All that's left is the test.

--- app/controllers/main_pages_controller.rb
+++ app/controllers/main_pages_controller.rb
@@ -3,7 +3,17 @@ class MainPagesController < ApplicationController
   
   def data_nuke
     if ryan?
-      standard('DATA DUMP - ' + Time.now, "")
+      data = ""
+      
+      # Dumping the relevant data.
+      Subject.all.each do |subject|
+        data += subject.to_data
+      end
+      Blog.all.each do |blog|
+        data += blog.to_data
+      end
+      
+      standard('DATA DUMP - ' + Time.now.to_s, data)
     else
     end
   end
