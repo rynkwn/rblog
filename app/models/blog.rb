bd4408662a472b040bbb7682ff27f8b2337ee493
rynkwn <rynkwn@gmail.com>
2015-10-03 06:29:02 +0000

    Removed to_data functions.

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -12,17 +12,4 @@ class Blog < ActiveRecord::Base
       self.content
     end
   end
-  
-  # to_data converts the meaningful data of this object into a string, to be used
-  # for data_nukes. ʭ is the delimiter between attributes, ʬ delimits objects.
-  def to_data
-    "Type: " + "Blog" + "ʭ" +
-    "Name: " + self.name + "ʭ" +
-    "Date Created: " + self.date_created.to_s + "ʭ" +
-    "Subject: " + Subject.find_by(id: self.subject_id).name + "ʭ" +
-    "Content: " + self.content + "ʭ" +
-    "Tags: " + self.tags.to_s +
-    "ʬ"
-  end
-  
 end
--- app/models/subject.rb
+++ app/models/subject.rb
@@ -2,12 +2,4 @@ class Subject < ActiveRecord::Base
   validates :name, presence: true
   
   has_many :blogs
-  
-  # to_data converts the meaningful data of this object into a string, to be used
-  # for data_nukes. ʭ is the delimiter between attributes, ʬ delimits objects.
-  def to_data
-    "Type: " + "Subject" + "ʭ" +
-    "Name: " + self.name +
-    "ʬ"
-  end
 end
