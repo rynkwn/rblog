03e2be2ec429f5acb2761763f07f32c53f282fbf
rynkwn <rynkwn@gmail.com>
2015-10-22 04:14:30 +0000

    Crush's now have their names downcased before they're saved.

--- app/models/crush.rb
+++ app/models/crush.rb
@@ -1,7 +1,13 @@
 class Crush < ActiveRecord::Base
+  before_save {self.name = name.downcase }
   
   def increment_fans
     self.fans += 1
     self.save!
+  end
+  
+  # Returns name with first letter of each "word" capitalized.
+  def get_name
+    self.name.titleize
   end
 end
