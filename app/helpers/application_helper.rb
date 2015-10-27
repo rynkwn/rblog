2c5e80ae34a5d264934e0ea6825eba8cbeb027ca
rynkwn <rynkwn@gmail.com>
2015-10-27 04:03:56 +0000

    Created hit function.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,10 +3,13 @@ module ApplicationHelper
   # Creates the title on a per-page basis
   def full_title(page_title = '')
     base_title = "aRg"
-    if page_title.empty?
-      base_title
+    
+    if page_title.empty?  # I'm repeating the hit function here. Can I avoid that?
+      hit(base_title)
+      return base_title
     else
-      page_title
+      hit(page_title)
+      return page_title
     end
   end
   
@@ -29,5 +32,12 @@ module ApplicationHelper
                                       fenced_code_blocks: true,   
                                       disable_indented_code_blocks: true)
     return markdown.render(text).html_safe
+  end
+  
+  private
+  
+  # Saves the "Hit" to the database.
+  def hit(page_desc)
+    Hits.create(page: page_desc, date_created: Time.now)
   end
 end
