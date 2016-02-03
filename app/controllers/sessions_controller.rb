5de794c504f0a1496f2260c15cd8310fbc5036b2
rynkwn <rynkwn@gmail.com>
2016-02-03 23:43:30 +0000

    Started playing around with immediately redirecting to certain path upon login.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,14 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       flash.now[:success] = 'Welcome!'
       log_in user
-      redirect_to root_path
+      
+      debugger
+      
+      # If there was an intended route, redirect there.
+      if params[:intended_route]
+      else
+        redirect_to root_path
+      end
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
