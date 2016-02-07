efaf13ca1c1d65e1edaabd7c808964c9cff51508
rynkwn <rynkwn@gmail.com>
2016-02-07 05:17:53 +0000

    More tampering with the checkboxes. Now arguably ready for some use.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -11,11 +11,9 @@ module UsersHelper
     # Personal Financials
     # Startups and Entrepreneurship 
     words["Internships and Jobs"] = "internship,job,resume,apply,program,recruiting,recruit,recruitment,part-time"
-    
     words["Colloquiums"] = "colloquium"
     
     words["Startups and Entrepreneurship"] = "startup,entrepreneur"
-    
     words["A cappella"] = "ephoria,accidentals,octet,springstreeters,good question,ephlats,elizabethans,aristocows"
     
     return words
