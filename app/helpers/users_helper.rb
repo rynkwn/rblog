075e8b0fb61568da52f71c14efc5aa4083b8642b
rynkwn <rynkwn@gmail.com>
2016-02-08 14:47:09 +0000

    Added more options to select for My Daily Messenger.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -10,11 +10,17 @@ module UsersHelper
     # A capella
     # Personal Financials
     # Startups and Entrepreneurship 
-    words["Internships and Jobs"] = "internship,job,resume,apply,program,recruiting,recruit,recruitment,part-time"
+    words["Internships and Jobs"] = "internship,job,resume,apply,program,recruiting,recruit,recruitment,part-time,route 2"
     words["Colloquiums"] = "colloquium"
     
     words["Startups and Entrepreneurship"] = "startup,entrepreneur"
     words["A cappella"] = "ephoria,accidentals,octet,springstreeters,good question,ephlats,elizabethans,aristocows"
+    
+    words["Competitions"] = "prize,compete,competition"
+    words["Food! (& Meals)"] = "dinner,lunch,pizza"
+    
+    words["Literature/Magazines"] = "magazine,prose,short stories,poetry,literary,literary magazine"
+    words["Music (Events, Auditions)"] = "audition,singing,cello,cellist,violist,viola,violin,quartet,quintet,tune,bass"
     
     return words
   end
