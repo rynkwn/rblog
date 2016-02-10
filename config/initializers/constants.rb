c91087f2555a46febbc2fee6db1d47538ce3f872
rynkwn <rynkwn@gmail.com>
2016-02-10 16:26:46 +0000

    Moved Keywords and Senders to constants file.

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -5,3 +5,26 @@ UNTRACKED_PAGES = %w[ Analytics Admin ]
 # Postmark Constants
 POSTMARK_API_KEY = "94252afc-de91-441e-9d0b-495a6ccadd42"  # (Manually inserted into application.rb)
 SENDER_SIGNATURE = 'rhk1@williams.edu'
+    
+# Daily Message Constants
+DAILY_MESSENGER_KEYWORDS = {
+  "Internships and Jobs" => "internship,job,apply,program,recruiting,recruit,recruitment,part-time,route 2",
+  "Startups and Entrepreneurship" => "startup,entrepreneur",
+  "A cappella" => "ephoria,accidentals,octet,springstreeters,good question,ephlats,elizabethans,aristocows",
+  "Competitions" => "prize,compete,competition",
+  "Food! (& Meals)" => "dinner,lunch,pizza",
+  "Literature/Magazines" => "magazine,prose,short stories,poetry,literary,literary magazine",
+  "Music (Events, Auditions)" => "audition,singing,cello,cellist,violist,viola,violin,quartet,quintet,tune,bass"
+}
+
+DAILY_MESSENGER_SENDERS = {
+  "Mathematics and Statistics" => "mathematics,statistics",
+  "Department of Biology" => "biology department",
+  "Department of Chemistry" => "chemistry department",
+  "Department of Psychology" => "psychology department",
+  "Career Center" => "career center",
+  "Office of Financial Aid" => "financial aid",
+  "Davis Center" => "davis center",
+  "Williams College Museum of Art" => "wcma,williams college museum of art",
+  "Athletics Department" => "athletics"
+}
