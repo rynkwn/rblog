876503da91e3be29b42d88268ecbeea4bc3227bd
rynkwn <rynkwn@gmail.com>
2016-02-10 15:27:18 +0000

    Decided I don't want to store old daily messages in strictly lower-cased letters.

--- app/models/daily_message.rb
+++ app/models/daily_message.rb
@@ -1,6 +1,4 @@
 class DailyMessage < ActiveRecord::Base
-  before_save {self.content = content.downcase }
-  
   after_initialize :trim_daily_messages
   
   # I only want to store the 6 most recent Daily Messages
