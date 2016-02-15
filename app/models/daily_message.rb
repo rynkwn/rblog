81bd60fc5ba65713940a7906d8013cea21c2f826
rynkwn <rynkwn@gmail.com>
2016-02-15 16:07:35 +0000

    Now only storing the last 3 Daily Messages, rather than the last 6.

--- app/models/daily_message.rb
+++ app/models/daily_message.rb
@@ -1,9 +1,9 @@
 class DailyMessage < ActiveRecord::Base
   after_initialize :trim_daily_messages
   
-  # I only want to store the 6 most recent Daily Messages
+  # I only want to store the 3 most recent Daily Messages
   def trim_daily_messages
-    if(DailyMessage.count > 6)
+    if(DailyMessage.count > 3)
       DailyMessage.destroy(DailyMessage.first.id)
     end
   end
