b79687f5b58172ea8d4ea9eb302d99b523e50ea8
rynkwn <rynkwn@gmail.com>
2016-04-17 16:57:41 +0000

    Nearly have this form thing figured out.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,16 @@
 module UsersHelper
+  
+  #############################################################
+  #
+  # Daily Messenger related functions
+  #
+  #############################################################
+  
+  def grab_keyword_values
+    if !@dm.adv_keywords.empty?
+      @dm.adv_keywords
+    else
+      
+    end
+  end
 end
