c5386e7ec704e1e694f68e1ebd4550da91912f1d
rynkwn <rynkwn@gmail.com>
2016-04-17 05:11:54 +0000

    Some more work done on the functional side of advanced functions.

--- db/migrate/20160401044357_add_adv_option_dm.rb
+++ db/migrate/20160401044357_add_adv_option_dm.rb
@@ -4,6 +4,7 @@ class AddAdvOptionDm < ActiveRecord::Migration
     add_column :service_dailies, :anti, :int, :default => 0  # Boolean.
     
     # Keys will be shared, and will denote topic/heading.
+    # Note. Each column below is internally represented and used as a hash.
     add_column :service_dailies, :adv_keywords, :text
     add_column :service_dailies, :adv_senders, :text
     add_column :service_dailies, :adv_categories, :text
