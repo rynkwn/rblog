d27de8281f013ab6255633a90e7aa843d36fa98d
rynkwn <rynkwn@gmail.com>
2016-04-19 15:47:14 +0000

    Drafted mockup of advanced form.

--- lib/daily_messenger_utils.rb
+++ lib/daily_messenger_utils.rb
@@ -1,11 +1,46 @@
 module DailyMessengerUtils
   
+  # Try to get the keywords associated with a key.
+  # @param key The key associated with the keywords
+  # @return if keywords are associated with that key, return it. Else
+  # return nil.
+  def DailyMessengerUtils.get_keywords(key)
+    DAILY_MESSENGER_KEYWORDS.fetch(key, nil)
+  end
+  
+  # Try to get the senders associated with a key.
+  # @param key The key associated with the sender information
+  # @return if sender are associated with that key, return it. Else
+  # return nil.
+  def DailyMessengerUtils.get_senders(key)
+    DAILY_MESSENGER_SENDERS.fetch(key, nil)
+  end
+  
   # Try to get the antiwords associated with a key.
   # Antiwords are deal breakers when we filter for this key.
-  # @param 
+  # @param key The key associated with the antiwords.
   # @return if antiwords are associated with that key, return it. Else
   # return nil.
   def DailyMessengerUtils.get_antiwords(key)
     DAILY_MESSENGER_ANTI_KEYWORDS.fetch(key, nil)
+  end
+  
+  # Try to get the category information associated with a key.
+  # @param key The key associated with the keywords
+  # @return if category information is associated with that key, return it. Else
+  # return nil.
+  def DailyMessengerUtils.get_category(key)
+    DAILY_MESSENGER_CATEGORY_MAPS.fetch(key, nil)
+  end
+  
+  # Given a category, surround it with "==="
+  def DailyMessengerUtils.format_category(category)
+    "=== " + category + " ==="
+  end
+  
+  # Given a category formatted like '=== STUFF ==='
+  # return 'STUFF'
+  def DailyMessengerUtils.unbox_category(category)
+    category.gsub("===", '').strip
   end
 end
